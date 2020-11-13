56e3af44f5850b151bf31dce5c86279c41acb46c
Israel <Israel.pro1994@gmail.com>
2020-11-13 12:58:14 -0600

    adding index and app to create react

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById("root")); // This is going to find our root that we defined in our index.html
